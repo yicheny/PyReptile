169b6b52f7e2b62896ab954c760dcc3dd1e83ec4
yicheny <1463020554@qq.com>
2019-07-11 13:18:10 +0800

    dilidili爬虫实现

--- DiliDili/demo2.py
+++ DiliDili/demo2.py
@@ -1,6 +1,8 @@
 import requests
 from bs4 import BeautifulSoup
 import time
+import xlwt
+import os
 
 # 标准网址格式http://www.dilidili.name/anime/urls/
 # 获取所有的urls
@@ -13,6 +15,7 @@ headers = {
 }
 
 urls = ['2000xqq','2000xq']
+datas = []
 def getUrls(url):
     urls.append(url)
     url = int(url)
@@ -37,16 +40,58 @@ def get_links(url):
         href = link.get('href')
         get_info(href,date)
 
+def get_value(v,*info):
+    if isinstance(v,list):
+        if(len(v)):
+            if(info):
+                return v[0].get(info[0])+ " | " + v[0].text
+            else:
+                return v[0].text
+        else:
+            return '此项信息缺失'
+    else:
+        return '此项信息缺失'
+
+i=0
 def get_info(url,date):
     url = ['http://www.dilidili.name{}/'.format(url)][0]
     wb_data = requests.get(url, headers=headers)
     soup = BeautifulSoup(wb_data.text, 'lxml')
-    tit = soup.find('h1')
+    tit = soup.select('h1')
     bdy = soup.select('li.list_xz>a')
+    data = {
+        'tit' : get_value(tit),
+        'date' : date,
+        'bdy' : get_value(bdy,'href'),
+        'url' : url
+    }
+    datas.append(data)
+    print('获取数据中...',len(datas))
+
+def save_to_execl(data,name,head):
+    try:
+        workbook = xlwt.Workbook(encoding='utf-8')
+        sheet = workbook.add_sheet(name)
+        for h in range(len(head)):
+            sheet.write(0, h, head[h])
+
+        i = 1
+        for obj in data:
+            j = 0
+            for v in (obj.values()):
+                sheet.write(i, j, v)
+                j += 1
+            i += 1
+        workbook.save('%s\execl\\%s.xls' % (os.getcwd(),name))
+        print('写入excel成功')
+    except Exception:
+        print('写入execl失败')
+
 
 if __name__ == '__main__':
     getUrls('201001')
     urls = ['http://www.dilidili.name/anime/{}/'.format(url) for url in urls]
     for single_url in urls:
         get_links(single_url)
-        time.sleep(2)
+        time.sleep(3)
+    save_to_execl(datas,'DiliDili',['番名','日期','百度云链接','详情网址'])
