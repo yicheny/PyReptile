4f74564233ac4247e142e1694dd9226828a3f144
yicheny <1463020554@qq.com>
2019-07-17 14:53:44 +0800

    初步接触mysql

--- segmentfault/s_demo1.py
+++ segmentfault/s_demo1.py
@@ -1,5 +1,7 @@
 import requests,datetime
 from requests.adapters import HTTPAdapter
+from lxml import etree
+
 
 headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.100 Safari/537.36'
@@ -10,12 +12,13 @@ start_time = datetime.datetime.now()
 s = requests.Session()
 s.mount('http://',HTTPAdapter(max_retries=15))
 s.mount('https://',HTTPAdapter(max_retries=15))
-url = 'https://segmentfault.com/'
+url = 'https://www.qidian.com/all'
 
 def get_content(url):
     res = s.get(url,headers=headers)
-    # res.encoding='gb2312'
-    print(res.text)
+    html = etree.HTML(res.text)
+    id = html.xpath('')
+    # print(res.text)
 
 
 if __name__ == '__main__':
