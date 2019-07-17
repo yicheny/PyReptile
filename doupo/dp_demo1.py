292640d0337e165950b81cc597d9a08f49943bc1
yicheny <1463020554@qq.com>
2019-07-17 09:58:59 +0800

    斗破苍穹爬取完成

--- doupo/dp_demo1.py
+++ doupo/dp_demo1.py
@@ -1,13 +1,9 @@
 import requests
-import os
-from bs4 import BeautifulSoup
 import re
-from lxml import etree
 import json
 import sys
 from requests.adapters import HTTPAdapter
 import datetime
-
 
 sys.setrecursionlimit(10000) #python递归默认最大深度为997,超过则会报错
 #注意，即使设的递归深度再大，也会受限于计算机本身的计算能力、系统版本、python版本等
@@ -29,27 +25,25 @@ params = {
     'cid':873530 # 873530-875451
 }
 
-def get_tits():
-    url = 'https://doupocangqiong1.com/1/'
-    res = requests.post(url,headers=headers)
-    res.encoding = 'utf-8'
-    selector = etree.HTML(res.text)
-    tits = selector.xpath('/html/body/section/div[3]/div[2]/ul/li/a[@title]/text()')
+# def get_tits():
+#     url = 'https://doupocangqiong1.com/1/'
+#     res = requests.post(url,headers=headers)
+#     res.encoding = 'utf-8'
+#     selector = etree.HTML(res.text)
+#     tits = selector.xpath('/html/body/section/div[3]/div[2]/ul/li/a[@title]/text()')
 
 params_cid = [(i) for i in range(873530,875452)]
 
-f = open('./source/斗破苍穹.txt','a+')
+f = open('./source/斗破苍穹.txt','a+',encoding='utf-8')#文件写入时，windows打开文件默认以“gbk“编码的，造成不识别unicode字符
 def get_content(cid):
     params['cid'] = cid
-    print(params['cid'])
-    # res = s.post(content_url,params,headers=headers)
-    # info = json.loads(res.text)['info']
-    # info = re.sub('<br>','',info)
-    # f.write(info)
-    # print(info)
+    res = s.post(content_url,params,headers=headers)
+    info = json.loads(res.text)['info']
+    info = re.sub('<br>','',info)
+    print('写入资源中...  ' + str(cid))
+    f.write(info)
 
 # tits = get_tits()
-
 
 if __name__ == '__main__':
     for i in params_cid:
