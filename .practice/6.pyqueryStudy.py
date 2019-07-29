012e25651ee1dac82a3f88d6b079e8b8cc163ed5
yicheny <1463020554@qq.com>
2019-07-29 14:53:31 +0800

    pyquery学习

--- .practice/6.pyqueryStudy.py
+++ .practice/6.pyqueryStudy.py
@@ -1,10 +1,13 @@
 from PyReptile.common.utils import get_page
 from pyquery import PyQuery as pq
-import re
 
 def get_pansoso_info():
     res = pq(get_page('http://www.pansoso.com/zh/hello'))
+
     item_list = res('div#content>div.pss')
+    currentPage = res('#sopage>a.this').text()
+    totalPage = res('#sopage>a').text().split(' ')
+
     for item in item_list.items():
         tits = item('h2>a')
         des = item('div.des').text().split(',')
@@ -18,7 +21,9 @@ def get_pansoso_info():
             'shareDate': des[3],
             'browerCount': des[4],
         }
-        print(info)
+        # print(info)
+
+    print(totalPage)
 
 
 if __name__ == '__main__':
