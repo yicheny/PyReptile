1b17310f5f06d047f416f5582a65e46043c4bd90
YLF <1463020554@qq.com>
2019-08-25 23:13:40 +0800

    mongoDB增加新的自定义方法

--- avmoo/total.py
+++ avmoo/total.py
@@ -6,22 +6,26 @@ from PyReptile.avmoo.setting import DB_NAME,TABLE_NAME
 class DataTotal():
     def __init__(self):
         self.db = MongoClient(db_name=DB_NAME, table_name=TABLE_NAME)
-        infos = self.db.find({})  # 获取数据库信息
-        self.stars = [] # 定义演员池
-        self.cavalry_movies = []  # 定义骑兵影片池
-        self.infantry_movies = []  # 定义步兵影片池
-        for i, o in enumerate(infos):
-            self.stars.append(o)
-            self.cavalry_movies.extend(o['cavalry_movies'])
-            self.infantry_movies.extend(o['infantry_movies'])
+        self.db2 = MongoClient(db_name=DB_NAME, table_name='avmoo_cavalry_movies')
+        self.db3 = MongoClient(db_name=DB_NAME, table_name='avmoo_infantry_movies')
+        self.infos = self.db.find({})
+        # self.save_movies()
+
+    def save_movies(self):
+        for i in enumerate(self.infos):
+            item = list(i)[1]
+            self.db2.add_many(item['cavalry_movies'])
+            self.db3.add_many(item['infantry_movies'])
 
 if __name__ == '__main__':
     start_time = time.time()
 
     data = DataTotal()
-    print('演员数目',len(data.stars))
-    print('骑兵影片数目',len(data.cavalry_movies))
-    print('步兵影片数目',len(data.infantry_movies))
+    print('演员数目',data.db.count())
+    # print('演员列表',data.db.distinct('name'))
+    print('骑兵影片数目',data.db2.count())
+    print('步兵影片数目',data.db3.count())
+    print('影片总数目',data.db2.count() + data.db3.count())
 
     end_time = time.time()
     print('执行时间：' + str(end_time-start_time))
--- common/mongoDB.py
+++ common/mongoDB.py
@@ -1,43 +1,87 @@
 MONGO_URI = 'localhost'
-DB_NAME = 'avmoo'#数据库名
-TABLE_NAME = 'avmoo_stars'#表名
+DB_NAME = 'avmoo'  # 数据库名
+TABLE_NAME = 'avmoo_stars'  # 表名
 DB_PORT = 27017
 
 import pymongo
 
+
 class MongoClient(object):
-    def __init__(self,host='localhost', port=DB_PORT,db_name=DB_NAME,table_name=TABLE_NAME):
-        self.db = pymongo.MongoClient(host=host,port=port)[db_name]
+    def __init__(self, host='localhost', port=DB_PORT, db_name=DB_NAME, table_name=TABLE_NAME):
+        self.db = pymongo.MongoClient(host=host, port=port)[db_name]
         self.table = self.db[table_name]
 
-    def change_table(self,table):
+    def change_table(self, table):
         self.table = self.db[table]
 
-    def add_one(self,obj):
+    def add_one(self, obj):
         self.table.insert_one(obj)
 
-    def find(self,condition):
+    def add_many(self, Arr):
+        if(not len(Arr)): return None
+        self.table.insert_many(Arr)
+
+    def find(self, condition):
         return self.table.find(condition)
 
-    def find_one(self,condition):
+    def find_one(self, condition):
         return self.table.find_one(condition)
 
+    def create_index(self, condition):
+        return self.table.create_index(condition)
+
+    def index_information(self):
+        return self.table.index_information()
+
+    def count(self):
+        return self.table.count()
+
+    def distinct(self,key):
+        return self.table.distinct(key)
+
+    def aggregate(self,pipeline):
+        return self.table.aggregate(pipeline)
+
+    # def list_indexes(self):
+    #     return self.table.list_indexes()
 
 
 mockData = {
-    'name':'lihua',
-    'age':28
+    'name': 'lihua',
+    'age': 28
 }
 mockData2 = {
-    'name':'lihua',
-    'age':319
+    'name': 'lihua',
+    'age': 319
 }
+mockData3 = [{
+            'name': 'lj',
+            'age': 234
+        },
+        {
+            'name': 'df',
+            'age':124
+        }]
+aggregateCond = [
+    # {'$group':{'_id':None,'count':{'$sum':1}}}
+    # {'$group':{'_id':'$cup','total':{'$sum':1}}}
+    # {'$group':{'_id':'$age','total':{'$sum':1}}}
+    # {'$group':{'_id':None,'total':{'$max':'$age'}}},
+    # {'$group': {'_id': None, 'total': {'$min':'$age'}}}
+]
 if __name__ == '__main__':
-    db = MongoClient()
-    db.add_one(mockData)
-    db.add_one(mockData2)
-    print(db.find_one({'name':'huahuahua'}))
-    res = db.find({'name':'huahuahua'})
-    print(41,res)
-    for i in res:
-        print(43,i)
+    db = MongoClient(db_name='demo_db',table_name='demo_table')
+    # db.add_many(mockData3)
+    # db = MongoClient()
+    # db.create_index('name')
+    # print(db.count())
+    # print(db.aggregate(aggregateCond).__dict__)
+    # print(db.aggregate(aggregateCond)._CommandCursor__data)
+    # print(db.distinct('name'))
+    # db.add_one(mockData)
+    # db.add_one(mockData2)
+    # print(db.find_one({'name':'huahuahua'}))
+    # res = db.find({'name':'huahuahua'})
+    # print(41,res)
+    # for i in res:
+    #     print(43,i)
